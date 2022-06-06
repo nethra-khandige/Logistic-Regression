a52203ef787ca01923a35240bcc2047d8258c5c9
Nethra Khandige <106540489+nethra-khandige@users.noreply.github.com>
2022-06-06 22:28:38 +0530

    Update and rename part6.py to part5.py

--- part5.py
+++ part5.py
@@ -6,6 +6,9 @@ from sklearn import preprocessing
 %matplotlib inline 
 import matplotlib.pyplot as plt
 
+df=pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv")
+churn_df = df[['ed', 'employ', 'equip', 'callcard','wireless','age','address','longmon']]
+
 #wireless
 churn_df = df[['ed', 'employ', 'equip', 'callcard','wireless','age','address','longmon','confer','cardmon','tollten']]
 churn_df['wireless'] = churn_df['wireless'].astype('int')
